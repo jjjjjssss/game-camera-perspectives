5d4c99a1086ab7af469f8515bca9fc6f127f4f90
Joyce Shen <jshen315@gatech.edu>
2023-02-09 03:37:39 -0500

    Add third person view

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -5,52 +5,61 @@ using UnityEngine;
 public class ThirdPersonController : MonoBehaviour
 {
 
-    // [SerializeField] private Rigidbody _rb;
-    // [SerializeField] private float _speed = 5;
-    // private Vector3 _input;
+    public float forwardSpeed = 5f;
+    public float sidewaySpeed = 5f;
+    public float turnSpeed = 360f;
+    public bool rotateWhenStill = true; 
 
-    // void Update() {
-    //     GatherInput();
-    // }
+    [SerializeField] private Transform cameraTransform;
 
-    // void FixedUpdate() {
-    //     Move();
-    // }
-
-    // void GatherInput() {
-    //     _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-    // }
-
-    // void Move() {
-    //     _rb.MovePosition(transform.position + transform.forward * _speed * Time.deltaTime);
-    // }
-
-    public float speed = 5f;
-    Vector3 forward;
-    Vector3 right;
 
     void Start() 
     {
-        forward = new Vector3(0, 0, 1); // forward is the direction of the camera
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
     }
+
 
     void Update() 
     {
-        if (Input.anyKey)
-        {
-            Move();
-        }
+        Move();
     }
+
 
     void Move()
     {
-        Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxis("Horizontal");
-        Vector3 forwardMovement = forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
-        Vector3 direction = Vector3.Normalize(forwardMovement + rightMovement);
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
 
-        transform.forward = direction; //rotate to face in direction of movement
-        transform.position += rightMovement;
-        transform.position += forwardMovement;
+        Quaternion direction = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up);
+        if (rotateWhenStill || vInput > 0f || hInput > 0f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, direction, turnSpeed * Time.deltaTime);
+        }
+
+        if (vInput != 0f)
+        {
+            Vector3 forwardMovement = direction * new Vector3(0, 0, 1) * vInput * forwardSpeed * Time.deltaTime;
+            transform.position += forwardMovement;
+        }
+
+        if (hInput != 0f)
+        {
+            Quaternion hDirection = Quaternion.Euler(new Vector3(0, 90, 0)) * direction;
+            Vector3 sidewayMovement = hDirection * new Vector3(0, 0, 1) * hInput * sidewaySpeed * Time.deltaTime;
+            transform.position += sidewayMovement;
+        }
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+
+        }
     }
 }
